33d202512a25b12fdb21d8f9cafcabedc53297ae
HuascarBaptista <huascar.baptista@rappi.com>
2020-08-17 20:23:56 -0500

    infinite

--- main.go
+++ main.go
@@ -75,5 +75,5 @@ func main() {
 	// The exit code of your Step is very important. If you return
 	//  with a 0 exit code `bitrise` will register your Step as "successful".
 	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+	os.Exit(1)
 }
